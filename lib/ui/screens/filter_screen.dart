221b4a5bf9645238c2ef927f778a693056b17f61
ilia volkov <Ilya.Volkov@noveogroup.com>
2021-03-11 17:33:41 +0300

    [lesson-7][IV] - changed layout for buildGridItem

--- lib/ui/screens/filter_screen.dart
+++ lib/ui/screens/filter_screen.dart
@@ -51,50 +51,53 @@ Widget _buildCocktailList() {
       valueListenable: _category,
       builder: (BuildContext context, CocktailCategory value, Widget child) {
         return FutureBuilder<Iterable<CocktailDefinition>>(
-            future: AsyncCocktailRepository().fetchCocktailsByCocktailCategory(_category.value),
+            future: AsyncCocktailRepository()
+                .fetchCocktailsByCocktailCategory(_category.value),
             builder: (BuildContext context, snapshot) {
-          if(snapshot.hasData){
-            var cocktailItems = snapshot.data;
-            var gridItems =
-            cocktailItems.map((item) => _buildGridItem(item)).toList();
-            return GridView.count(
-              crossAxisCount: 2,
-              childAspectRatio: 1.0,
-              mainAxisSpacing: 8.0,
-              crossAxisSpacing: 8.0,
-              children: gridItems,
-            );
-          }
-          if(snapshot.hasError){
-            return Center(child: Text(
-              'Сообщение об ошибке',
-              style: const TextStyle(
-                fontWeight: FontWeight.w400,
-                color: Color(0xffEAEAEA),
-              ),
-            ),);
-          }
-          return Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Image.asset(
-                  "assets/shaker.png",
-                ),
-                Container(
-                  margin: const EdgeInsets.only(top: 9),
-                  child: const Text(
-                    'Поиск...',
+              if (snapshot.hasData) {
+                var cocktailItems = snapshot.data;
+                var gridItems =
+                    cocktailItems.map((item) => _buildGridItem(item)).toList();
+                return GridView.count(
+                  crossAxisCount: 2,
+                  childAspectRatio: 1.0,
+                  mainAxisSpacing: 8.0,
+                  crossAxisSpacing: 8.0,
+                  children: gridItems,
+                );
+              }
+              if (snapshot.hasError) {
+                return Center(
+                  child: Text(
+                    'Сообщение об ошибке',
                     style: const TextStyle(
                       fontWeight: FontWeight.w400,
                       color: Color(0xffEAEAEA),
                     ),
                   ),
+                );
+              }
+              return Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Image.asset(
+                      "assets/shaker.png",
+                    ),
+                    Container(
+                      margin: const EdgeInsets.only(top: 9),
+                      child: const Text(
+                        'Поиск...',
+                        style: const TextStyle(
+                          fontWeight: FontWeight.w400,
+                          color: Color(0xffEAEAEA),
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
-              ],
-            ),
-          );
-        });
+              );
+            });
       });
 }
 
@@ -168,48 +171,68 @@ class _CategoryListState extends State<CategoryList> {
   }
 }
 
-
 Widget _buildGridItem(CocktailDefinition cocktail) {
   return Container(
     child: Stack(
       children: [
-        Image.network(cocktail.drinkThumbUrl),
-        Align(
-          alignment: Alignment.bottomLeft,
-          child: Container(
-            padding: const EdgeInsets.only(left: 16, top: 13),
-            width: 160,
-            height: 94,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  cocktail.name,
-                  textAlign: TextAlign.left,
-                  style: const TextStyle(
-                    fontSize: 14.0,
-                    fontWeight: FontWeight.w500,
-                    color: Colors.white,
-                  ),
-                ),
-                Container(
-                  padding: const EdgeInsets.only(left: 16, top: 6),
-                  margin: const EdgeInsets.only(top: 7),
-                  child: Text(
-                    cocktail.id,
-                    textAlign: TextAlign.center,
-                    style: const TextStyle(
-                      fontSize: 10.0,
-                      fontWeight: FontWeight.w400,
-                      color: Colors.white,
-                    ),
-                  ),
-                ),
-              ],
+        Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: NetworkImage(cocktail.drinkThumbUrl),
             ),
+            borderRadius: BorderRadius.circular(8.0),
           ),
         ),
+        Container(
+          padding: EdgeInsets.symmetric(vertical: 16.0, horizontal: 14.0),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(8.0),
+            gradient: LinearGradient(
+                begin: Alignment.bottomCenter,
+                end: Alignment.topCenter,
+                colors: [
+                  Colors.black.withOpacity(1.0),
+                  Colors.black.withOpacity(0.0),
+                ]),
+          ),
+          child: Align(
+            alignment: Alignment.bottomLeft,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(cocktail.name, style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.w400,
+                ),),
+                _cocktailInfoBadge("ID ${cocktail.id}")
+              ],
+            ),
+          ) ,
+        ),
+        // ClipRRect(
+        //     borderRadius: BorderRadius.circular(8.0),
+        //     child: Image.network(cocktail.drinkThumbUrl)),
       ],
+    ),
+  );
+}
+
+
+Widget _cocktailInfoBadge(String text) {
+  return Container(
+    padding: EdgeInsets.symmetric(vertical: 6.0, horizontal: 16.0),
+    margin: EdgeInsets.only(top: 8.0),
+    decoration: BoxDecoration(
+      color: Color(0xff15151C),
+      borderRadius: BorderRadius.circular(30.0),
+    ),
+    child: Text(
+      text,
+      style: TextStyle(
+        fontSize: 10,
+        color: Color(0xffEAEAEA),
+      ),
     ),
   );
 }
